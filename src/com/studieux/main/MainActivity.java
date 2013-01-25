08850c0d98fa176a8dd0eb5a619be399960c23dd
Louis D <link14.online@gmail.com>
2013-01-25 17:19:08 +0100

    ajout xml, ajout sliding menu

--- src/com/studieux/main/MainActivity.java
+++ src/com/studieux/main/MainActivity.java
@@ -1,7 +1,11 @@
 package com.studieux.main;
 
+import com.slidingmenu.lib.SlidingMenu;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +14,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		int width = size.x;
+		int behindOffset = (int) (width - (0.5*width));
+		
+		SlidingMenu menu = new SlidingMenu(this);
+		menu.setMode(SlidingMenu.LEFT);
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.sliding_menu);
+        menu.setBehindOffset(behindOffset);
 	}
 
 	@Override
